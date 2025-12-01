5246bfa523cb6d6575a74f1704f03a27bc744507
VikkMoor <moorvikk@gmail.com>
2025-12-01 20:33:11 +0500

    addContact

--- src/main/java/basicsHW/ContactsAccounting.java
+++ src/main/java/basicsHW/ContactsAccounting.java
@@ -6,7 +6,10 @@ public class ContactsAccounting {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int capacity = 50;
         int contactCount = 0;
+        String[] names = new String[capacity];
+        String[] phoneNumbers = new String[capacity];
 
         while (true) {
             System.out.println("\nМеню:");
@@ -19,7 +22,41 @@ public class ContactsAccounting {
             int choice = scanner.nextInt();
             scanner.nextLine(); // очистка буфера
 
-
+            switch (choice) {
+                case 1:
+                    addContact();
+                    break;
+                case 2:
+                    listContacts();
+                    break;
+                case 3:
+                    searchContact();
+                    break;
+                case 4:
+                    removeContact();
+                    break;
+                case 5:
+                    System.out.println("Завершение работы.");
+                    return;
+                default:
+                    System.out.println("Выберите вариант из предложенных");
+            }
         }
+
+        public void addContact() {
+            if (contactCount >= capacity) {
+                System.out.println("Достигнуто максимальное количество контактов.");
+                return;
+            }
+            System.out.print("Введите имя контакта: ");
+            names[contactCount] = scanner.nextLine();
+            System.out.print("Введите телефонный номер: ");
+            phoneNumbers[contactCount] = scanner.nextLine();
+            contactCount++;
+            System.out.println("Контакт добавлен.");
+        }
+
+
+
     }
 }
